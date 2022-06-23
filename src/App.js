1d917d3bd86f4785babbbb7dfa59192a6da7d3a1
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-23 10:59:10 +0200

    add test 1 for header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Counter from "./counter/Counter";
 
 function App() {
   return (
     <div className="App">
-    
+      <Counter />
     </div>
   );
 }
