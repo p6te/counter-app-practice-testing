f8c44e08fa0aa0fd1099151d9c79eb42d9eadb9c
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-23 11:25:38 +0200

    test 2 - counter value, 3 - input value, 4/5 -btns

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Counter.css";
 
 function Counter() {
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  
   return (
     <div>
       <h1 data-testid="header">My Counter</h1>
+      <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="subtract-btn">-</button>
+      <input type="number" data-testid="input" value={inputValue} />
+      <button data-testid="add-btn">+</button>
     </div>
   );
 }
